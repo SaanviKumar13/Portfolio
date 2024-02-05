c18786e208d82cdaa0072fdfe376a7dfd79eae11
SaanviKumar13 <saanviprashantkumar@gmail.com>
2024-02-05 23:33:26 +0530

    feat: add basic hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,10 +2,12 @@ const Hero = () => {
   return (
     <div className="flex m-10">
       <div className="text-left ml-48 mt-32">
-        <p className="text-xl text-[#0a2140] font-medium">Hi my name is</p>
-        <h1 className="text-5xl font-bold inline-block text-transparent bg-clip-text bg-gradient-to-r from-sky-100 via-slate-300 to-gray-200 ">
+        <h1 className="text-7xl mt-1 font-extrabold inline-block text-transparent bg-clip-text bg-gradient-to-r from-sky-100 via-slate-300 to-gray-200 ">
           Saanvi Kumar
         </h1>
+        <p className="text-xl font-semibold text-[#0a2140] ml-2 mt-2">
+          Every problem is a new project.
+        </p>
       </div>
     </div>
   );
