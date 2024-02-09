026b1d31002211d699e5b19ff70c85d0f81dc2dc
SaanviKumar13 <saanviprashantkumar@gmail.com>
2024-02-09 16:45:54 +0530

    feat: add title and favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,14 @@
 import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
 import Socials from "@/components/Socials";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <div className="bg-primary font-main shadow-page bg-page-gradient h-screen">
+      <Head>
+        <title>Saanvi Kumar</title>
+      </Head>
       <Navbar />
       <Socials />
       <Hero />
