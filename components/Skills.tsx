2daad805739cd9677d9a152a046976fa875c24ef
SaanviKumar13 <saanviprashantkumar@gmail.com>
2024-03-01 23:06:58 +0530

    feat: add skills section

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,12 +1,33 @@
+import { SkillList } from "@/utils/data";
+
+interface SkillProps {
+  image: string;
+  name: string;
+}
+
 const Skills = () => {
   return (
-    <div className="h-[50vh] ml-20 w-[80vw]">
-      <h1 className="text-5xl font-title font-extrabold text-left text-slate-100">
-        Skills
-      </h1>
-      <p className="center">
-        <img src="https://skillicons.dev/icons?i=git,kubernetes,docker,c,vim" />
-      </p>
+    <div className="flex flex-col justify-center items-center">
+      <div className="h-fit md:ml-20">
+        <h1 className="text-5xl font-title font-extrabold text-center mb-10 md:mb-0 text-slate-100">
+          Skills
+        </h1>
+        <div className="flex flex-wrap justify-center items-center my-10 mt-0">
+          {SkillList.map(({ image, name }: SkillProps) => {
+            return (
+              <div className="flex flex-col items-center justify-center text-white my-5 mx-16">
+                <img
+                  src={image}
+                  alt={name}
+                  key={image}
+                  className="p-2 sm:p-6 w-20 md:w-32"
+                />
+                <h1 className="font-bold">{name}</h1>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 };
--- utils/data.tsx
+++ utils/data.tsx
@@ -16,3 +16,82 @@ export const NavLink = [
     link: "#contact-me",
   },
 ];
+
+export const SkillList = [
+  {
+    name: "Python",
+    image:
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original.svg",
+  },
+  {
+    name: "TypeScript",
+    image:
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg",
+  },
+  {
+    name: "Java",
+    image:
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/java/java-original.svg",
+  },
+  {
+    name: "C++",
+    image:
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/cplusplus/cplusplus-original.svg",
+  },
+  {
+    name: "HTML",
+    image:
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg",
+  },
+  {
+    name: "CSS",
+    image:
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg",
+  },
+  {
+    name: "TailwindCSS",
+    image:
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg",
+  },
+  {
+    name: "SASS",
+    image:
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/sass/sass-original.svg",
+  },
+  {
+    name: "React",
+    image:
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg",
+  },
+  {
+    name: "Next.js",
+    image:
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nextjs/nextjs-original.svg",
+  },
+
+  {
+    name: "Matplotlib",
+    image:
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/matplotlib/matplotlib-original-wordmark.svg",
+  },
+  {
+    name: "Pandas",
+    image:
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/pandas/pandas-original-wordmark.svg",
+  },
+  {
+    name: "MatLab",
+    image:
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/matlab/matlab-original.svg",
+  },
+  {
+    name: "Tensorflow",
+    image:
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tensorflow/tensorflow-original.svg",
+  },
+  {
+    name: "ScikitLearn",
+    image:
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/scikitlearn/scikitlearn-original.svg",
+  },
+];
