c19e04c289ba8d54dcd2f58b07f801f6ccd95bfd
SaanviKumar13 <saanviprashantkumar@gmail.com>
2024-04-05 11:06:25 +0530

    fix: lint

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,7 +6,7 @@ const Hero = () => {
       <div className="z-10 flex flex-col h-[70vh] items-start justify-between gap-9 p-10 ml-20">
         <div className="mt-10 flex flex-col justify-evenly h-full">
           <h1 className="text-lg mt-1 font-extrabold text-slate-100">
-            👋 Hey, I'm Saanvi.
+            👋 Hey, I&apos;m Saanvi.
           </h1>
           <h2 className="text-2xl md:text-7xl font-mono font-extrabold text-slate-100 mt-2">
             I love <span className="text-[#BCD3E7] font-title">building</span>{" "}
@@ -16,7 +16,7 @@ const Hero = () => {
           </h2>
         </div>
         <div className="flex gap-5 md:gap-12">
-          <Button variant="secondary" children="Resume" />
+          <Button variant="secondary"> Resume </Button>
         </div>
       </div>
     </div>
