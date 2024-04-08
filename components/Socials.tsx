29e579b867315a4f63bf85ba2a4678473a42ffa4
SaanviKumar13 <saanviprashantkumar@gmail.com>
2024-04-08 10:59:51 +0530

    fix: socials bar

--- components/Socials.tsx
+++ components/Socials.tsx
@@ -1,6 +1,28 @@
-import { SocialData } from "@/utils/data";
 import Link from "next/link";
-
+import { BiLogoGmail } from "react-icons/bi";
+import { FaInstagram, FaGithub, FaLinkedin } from "react-icons/fa";
+const SocialData = [
+  {
+    name: "Instagram",
+    icon: <FaInstagram className="w-6 h-6 md:w-10 md:h-10 mb-2 md:my-2" />,
+    href: "https://www.instagram.com/saanviii.13/",
+  },
+  {
+    name: "GitHub",
+    icon: <FaGithub className="w-6 h-6 md:w-8 md:h-8 mb-2 md:my-2" />,
+    href: "https://github.com/SaanviKumar13",
+  },
+  {
+    name: "LinkedIn",
+    icon: <FaLinkedin className="w-6 h-6 md:w-8 md:h-8 mb-2 md:my-2" />,
+    href: "https://www.linkedin.com/in/saanvi-kumar-71481624b/",
+  },
+  {
+    name: "Gmail",
+    icon: <BiLogoGmail className="w-6 h-6 md:w-8 md:h-8 mb-2 md:my-2" />,
+    href: "mailto:saanviprashantkumar@gmail.com",
+  },
+];
 const Socials = () => {
   return (
     <div className="fixed right-3 md:right-5 bottom-0 z-30">
